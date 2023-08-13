2b257051a54379e55583f1898ca3098ac03ecad7
ravkamaria <hevkomaria1@gmail.com>
2023-08-13 17:11:24 +0300

    Task 3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,10 +5,12 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 let throttle = require('lodash.throttle');
 
-const keyCurrentTime = "videoplayer-current-time";
-const playedTime = function(data) {localStorage.setItem(keyCurrentTime, data.seconds)};
+const keyCurrentTime = 'videoplayer-current-time';
+const playedTime = function (data) {
+  localStorage.setItem(keyCurrentTime, data.seconds);
+};
 player.on('timeupdate', throttle(playedTime, 1000));
 
-const stopedTime = localStorage.getItem(keyCurrentTime);
+const stopedTime = localStorage.getItem(keyCurrentTime) ?? 0;
 
 player.setCurrentTime(stopedTime);
---
+++ src/js/03-feedback.js
@@ -0,0 +1,41 @@
+import throttle from 'lodash.throttle';
+
+let throttle = require('lodash.throttle');
+
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const KEY_DATA = 'feedback-form-state';
+const dataObj = {};
+
+form.addEventListener('input', throttle(inputData, 500));
+
+function inputData(evt) {
+  dataObj[evt.target.name] = evt.target.value;
+  localStorage.setItem(KEY_DATA, JSON.stringify(dataObj));
+}
+
+form.addEventListener('submit', submitData);
+
+function submitData(evt) {
+  evt.preventDefault();
+  const {
+    elements: { email, message },
+  } = evt.currentTarget;
+  if (email.value === '' || message.value === '') {
+    window.alert('Please, fill in all the fields');
+  } else {
+    console.log(JSON.parse(localStorage.getItem(KEY_DATA)));
+    form.reset();
+    localStorage.removeItem(KEY_DATA);
+  }
+}
+
+checkSavedData();
+function checkSavedData() {
+  const savedData = JSON.parse(localStorage.getItem(KEY_DATA));
+  if (savedData) {
+    input.value = savedData.email;
+    textarea.value = savedData.message;
+  }
+}
