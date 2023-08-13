5ffa02ae0ad08f16f313e4dcae4fdaff2bb84376
ravkamaria <hevkomaria1@gmail.com>
2023-08-13 18:48:16 +0300

    Task 03 fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,11 @@
 import throttle from 'lodash.throttle';
-
 let throttle = require('lodash.throttle');
 
 const form = document.querySelector('.feedback-form');
 const input = document.querySelector('input');
 const textarea = document.querySelector('textarea');
 const KEY_DATA = 'feedback-form-state';
-const dataObj = {};
+const dataObj = JSON.parse(localStorage.getItem(KEY_DATA)) ?? {};
 
 form.addEventListener('input', throttle(inputData, 500));
 
@@ -28,6 +27,7 @@ function submitData(evt) {
     console.log(JSON.parse(localStorage.getItem(KEY_DATA)));
     form.reset();
     localStorage.removeItem(KEY_DATA);
+    // localStorage.clear();
   }
 }
 
@@ -35,7 +35,7 @@ checkSavedData();
 function checkSavedData() {
   const savedData = JSON.parse(localStorage.getItem(KEY_DATA));
   if (savedData) {
-    input.value = savedData.email;
-    textarea.value = savedData.message;
+    input.value = savedData.email ?? "";
+    textarea.value = savedData.message ?? "";
   }
 }
