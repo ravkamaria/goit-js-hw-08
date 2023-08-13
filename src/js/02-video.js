42c1fab382066278566fc893385b4bce60b49e3e
ravkamaria <hevkomaria1@gmail.com>
2023-08-13 12:32:59 +0300

    Task 02 done

---
+++ src/js/02-video.js
@@ -0,0 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+let throttle = require('lodash.throttle');
+
+const keyCurrentTime = "videoplayer-current-time";
+const playedTime = function(data) {localStorage.setItem(keyCurrentTime, data.seconds)};
+player.on('timeupdate', throttle(playedTime, 1000));
+
+const stopedTime = localStorage.getItem(keyCurrentTime);
+
+player.setCurrentTime(stopedTime);
